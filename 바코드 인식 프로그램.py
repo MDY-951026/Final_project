7b7d82858e84d00b5c32c0866894e09ef768a009
MDY-951026 <gi86308630@gmail.com>
2022-10-25 10:02:50 +0900

    Add file for barcode detection

---
+++ 바코드 인식 프로그램.py
@@ -0,0 +1,21 @@
+import cv2
+import pyzbar.pyzbar as pyzbar
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, frame = cap.read()
+    
+    if success:
+        for code in pyzbar.decode(frame):
+            my_code = code.data.decode("utf-8")
+            print("인식 성공 : ",my_code)
+        cv2.imshow("cam",frame)
+        
+        key = cv2.waitKey(1)
+        if key ==27:
+                break
+                
+cap.release()
+cv2.destroyALLWindows()
+            
